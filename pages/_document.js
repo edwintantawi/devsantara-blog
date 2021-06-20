75c47e21d282cf5ad8d4a19261f58aec709b28b7
edwintantawi <edwintantawi@gmail.com>
2021-06-20 13:31:25 +0700

    link to google fonts

--- pages/_document.js
+++ pages/_document.js
@@ -84,6 +84,11 @@ class MyDocument extends Document {
           <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
           <meta name="theme-color" content="#001227" />
           <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
