88ab7970d0c77a27df46ad53c366975c51caa261
edwintantawi <edwintantawi@gmail.com>
2021-06-20 13:31:54 +0700

    create mediaquery

---
+++ styles/mediaQuery.js
@@ -0,0 +1,11 @@
+export const screen_size = {
+  sm: 640,
+  md: 768,
+  lg: 1024,
+  xl: 1280,
+  xxl: 1536,
+};
+
+const minWidth = (screen) => `(min-width: ${screen_size[screen]}px)`;
+
+export default minWidth;
