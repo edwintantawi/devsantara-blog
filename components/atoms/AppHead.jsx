ba1fea78532dde7d7e632e4aaebe5d439eee2169
edwintantawi <edwintantawi@gmail.com>
2021-06-22 15:55:17 +0700

    🔍  create AppHead component for SEO

---
+++ components/atoms/AppHead.jsx
@@ -0,0 +1,30 @@
+import Head from 'next/head';
+import {
+  appHeadPropTypes,
+  appHeadDefaultProps,
+} from '../../porpTypes/appHead.proptypes';
+
+const BASE_KEYWORDS =
+  'devsantara, Devsantara, DEVSANTARA, DevSantara, Developer Nusantara, blogs, programming, developer, software enginering, developer platform, indonesia, developer indonesia, development, software development, developer indonesia';
+
+const BASE_URL = 'https://devsantara.vercel.app';
+
+const AppHead = ({ title, keywords, description, url }) => {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="keywords" content={`${BASE_KEYWORDS}, ${keywords}`} />
+      <meta name="description" content={description} />
+      <meta property="og:title" content={`Devsantara | ${title}`} />
+      <meta property="og:url" content={`${BASE_URL}/${url}`} />
+      <meta property="og:image" content="/android-icon-192x192.png" />
+    </Head>
+  );
+};
+
+// proptypes
+AppHead.propTypes = appHeadPropTypes;
+
+AppHead.defaultProps = appHeadDefaultProps;
+
+export default AppHead;
