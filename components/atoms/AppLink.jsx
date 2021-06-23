10575dbb88c011fcf549dfb80e34630057e3d0cb
edwintantawi <edwintantawi@gmail.com>
2021-06-24 00:29:07 +0700

    ✨ create AppLink component

---
+++ components/atoms/AppLink.jsx
@@ -0,0 +1,47 @@
+import Link from 'next/link';
+import PropTypes from 'prop-types';
+import { useRouter } from 'next/router';
+import styled from 'styled-components';
+import Colors from '../../styles/colors';
+
+const AppLink = ({ title, href, Icon }) => {
+  const router = useRouter();
+  const active = router.asPath === href;
+
+  return (
+    <Link href={href} passHref>
+      <LinkLayout active={active}>
+        <Icon />
+        <TitleLayout active={active}>{title}</TitleLayout>
+      </LinkLayout>
+    </Link>
+  );
+};
+
+const LinkLayout = styled.a`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding: 0.8rem 1rem;
+  margin-bottom: 0.8rem;
+  border-radius: 6px;
+  text-decoration: none;
+  background-color: ${({ active }) =>
+    active ? Colors.darkBlue : Colors.white};
+  color: ${({ active }) => (active ? Colors.white : Colors.darkBlue)};
+  border-left: 0.35rem solid ${Colors.darkBlue};
+`;
+
+const TitleLayout = styled.div`
+  margin-left: 0.4rem;
+  font-size: 0.9rem;
+  font-weight: ${({ active }) => (active ? 700 : 500)};
+`;
+
+AppLink.propTypes = {
+  title: PropTypes.string.isRequired,
+  href: PropTypes.string.isRequired,
+  Icon: PropTypes.elementType.isRequired,
+};
+
+export default AppLink;
