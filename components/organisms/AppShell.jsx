4594374512c0fa8acb45a4a9cc79824cdc2ac0fb
edwintantawi <edwintantawi@gmail.com>
2021-06-23 23:17:55 +0700

    💄 add footer and style layout

--- components/organisms/AppShell.jsx
+++ components/organisms/AppShell.jsx
@@ -1,20 +1,43 @@
+import styled from 'styled-components';
 import AppHead from '../atoms/AppHead';
 import AppHeader from '../molecules/AppHeader';
+import AppWrapper from '../atoms/AppWrapper';
+import AppFooter from '../molecules/AppFooter';
 import { appHeadPropTypes } from '../../porpTypes/appHead.proptypes';
 
 const AppShell = ({ children, title, keyword, description, url }) => (
-  <>
-    <AppHead
-      title={title}
-      keyword={keyword}
-      description={description}
-      url={url}
-    />
-    <AppHeader />
-    <main>{children}</main>
-    <footer>footer</footer>
-  </>
+  <RootLayout>
+    <ViewLayout>
+      <AppHead
+        title={title}
+        keyword={keyword}
+        description={description}
+        url={url}
+      />
+      <AppHeader />
+      <AppWrapper>
+        <MainLayout>{children}</MainLayout>
+      </AppWrapper>
+    </ViewLayout>
+    <AppFooter />
+  </RootLayout>
 );
+
+const RootLayout = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ViewLayout = styled.div`
+  flex: 1;
+`;
+
+const MainLayout = styled.main`
+  display: grid;
+  grid-template-columns: auto 1fr;
+  column-gap: 32px;
+`;
 
 AppShell.propTypes = appHeadPropTypes;
 
