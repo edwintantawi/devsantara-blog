63eabf12df73358d9c6971c8d146ccd4d9e267b0
edwintantawi <edwintantawi@gmail.com>
2021-06-24 01:06:33 +0700

    🚀 use AppContextProvider

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,14 @@
 import GlobalStyle from '../styles/globalStyle';
 import DataContextProvider from '../context/dataContext/store';
+import AppContextProvider from '../context/appContext/store';
 
 const MyApp = ({ Component, pageProps }) => (
   <>
     <GlobalStyle />
     <DataContextProvider>
-      <Component {...pageProps} />
+      <AppContextProvider>
+        <Component {...pageProps} />
+      </AppContextProvider>
     </DataContextProvider>
   </>
 );
