e41b76468acc4ced01f3daddc16a45163c55a589
edwintantawi <edwintantawi@gmail.com>
2021-06-24 11:49:03 +0700

    ✨ add more navigation

--- components/molecules/AppNavigation.jsx
+++ components/molecules/AppNavigation.jsx
@@ -1,3 +1,6 @@
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import SettingsIcon from '@material-ui/icons/Settings';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import styled from 'styled-components';
 import HomeIcon from '@material-ui/icons/Home';
 import GpsFixedIcon from '@material-ui/icons/GpsFixed';
@@ -5,6 +8,7 @@ import GroupIcon from '@material-ui/icons/Group';
 import AppLink from '../atoms/AppLink';
 import minWidth from '../../styles/mediaQuery';
 import Colors from '../../styles/colors';
+import AppButton from '../atoms/AppButton';
 import useAppContext from '../../hooks/useAppContext';
 
 const AppNavigation = () => {
@@ -14,6 +18,12 @@ const AppNavigation = () => {
       <AppLink href="/" title="Home" Icon={HomeIcon} />
       <AppLink href="/guide" title="Guide" Icon={GpsFixedIcon} />
       <AppLink href="/about" title="About" Icon={GroupIcon} />
+      <AppLink href="/dashboard" title="Dashboard" Icon={DashboardIcon} />
+      <AppLink href="/settings" title="Settings" Icon={SettingsIcon} />
+      <AppButton type="button" buttonType="button" red full>
+        <ExitToAppIcon />
+        <span style={{ marginLeft: '0.3rem', fontWeight: '700' }}>Logout</span>
+      </AppButton>
     </NavLayout>
   );
 };
@@ -24,7 +34,7 @@ const NavLayout = styled.nav`
   right: 0;
   top: 51px;
   background-color: ${Colors.white};
-  padding: 1rem 1rem 0.2rem;
+  padding: 1rem;
   border-bottom: 3px solid ${Colors.darkBlue};
   box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
   transform: ${({ active }) =>
