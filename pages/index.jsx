df208f7aceba8b66e54be04c4b058913e6ac8afd
edwintantawi <edwintantawi@gmail.com>
2021-06-24 23:07:47 +0700

    🚧 try to use AppBlogCard

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,26 @@
 import AppShell from '../components/organisms/AppShell';
 import AppNavigation from '../components/molecules/AppNavigation';
+import AppBlogCard from '../components/atoms/AppBlogCard';
 
 const Home = () => (
   <AppShell>
     <AppNavigation />
-    <h1>Devsantara</h1>
+    <div>
+      {[0, 1, 2, 3, 4, 5].map((id) => (
+        <AppBlogCard
+          key={id}
+          index={id}
+          data={{
+            bannerImage: '/assets/dummy-post-banner.jpg',
+            authorName: 'markZuck',
+            timestamp: '02 Jan 2021',
+            title:
+              '30minutes Learn Next JS with firebase to build a blogs apps',
+            tags: ['javascript', 'nextjs', 'reactjs', 'firebase'],
+          }}
+        />
+      ))}
+    </div>
   </AppShell>
 );
 
