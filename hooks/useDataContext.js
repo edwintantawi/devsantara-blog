3de68df270f0007a99e41e89e9a1d25e3a05d018
edwintantawi <edwintantawi@gmail.com>
2021-06-25 18:11:17 +0700

    renamed

--- hooks/useDataContext.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import { AuthContext } from '../context/authContext/store';
-
-const useDataContext = () => useContext(AuthContext);
-
-export default useDataContext;
