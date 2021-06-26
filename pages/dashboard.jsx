3c1319b72fffe530e989dce4426c4c933fc11e85
edwintantawi <edwintantawi@gmail.com>
2021-06-27 01:33:04 +0700

    🚧 try use AppTextEditor

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,9 +1,11 @@
 import AppShell from '../components/organisms/AppShell';
 import AppNavigation from '../components/molecules/AppNavigation';
+import AppTextEditor from '../components/organisms/AppTextEditor';
 
 const Dashboard = () => (
   <AppShell>
     <AppNavigation />
+    <AppTextEditor />
   </AppShell>
 );
 
