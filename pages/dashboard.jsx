19ee3d65f48c9f1cf9ed7187ad8ba1499eefdb32
edwintantawi <edwintantawi@gmail.com>
2021-06-27 00:42:12 +0700

    🎨 create dashboard page

---
+++ pages/dashboard.jsx
@@ -0,0 +1,10 @@
+import AppShell from '../components/organisms/AppShell';
+import AppNavigation from '../components/molecules/AppNavigation';
+
+const Dashboard = () => (
+  <AppShell>
+    <AppNavigation />
+  </AppShell>
+);
+
+export default Dashboard;
