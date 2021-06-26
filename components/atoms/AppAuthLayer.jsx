3a810ba410770b22de6231fcc9abb96ce238e70c
edwintantawi <edwintantawi@gmail.com>
2021-06-27 00:06:53 +0700

    🎨 add loading state

--- components/atoms/AppAuthLayer.jsx
+++ components/atoms/AppAuthLayer.jsx
@@ -1,10 +1,13 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import useAuthContext from '../../hooks/useAuthContext';
 import { auth } from '../../lib/firebase';
 import ACTION_TYPES from '../../context/actionTypes';
+import AppLoading from './AppLoading';
 
 const AppAuthLayer = () => {
   const [, dispatchAuth] = useAuthContext();
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     auth.onAuthStateChanged((result) => {
       if (result) {
@@ -19,11 +22,13 @@ const AppAuthLayer = () => {
           type: ACTION_TYPES.SET_AUTH,
           payload: userAuthData,
         });
+        setIsLoading(false);
       }
+      setIsLoading(false);
     });
   }, [dispatchAuth]);
 
-  return null;
+  return isLoading ? <AppLoading /> : null;
 };
 
 export default AppAuthLayer;
