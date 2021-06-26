f85fb82abdb952fff4f817d754363a1731f830b5
edwintantawi <edwintantawi@gmail.com>
2021-06-27 00:05:51 +0700

    🎨 remove login from header

--- components/molecules/AppHeader.jsx
+++ components/molecules/AppHeader.jsx
@@ -1,44 +1,22 @@
 import styled from 'styled-components';
-import Image from 'next/image';
 import { IconButton } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
 import Avatar from '@material-ui/core/Avatar';
 import MenuIcon from '@material-ui/icons/Menu';
 import AppLogo from '../atoms/AppLogo';
-import AppButton from '../atoms/AppButton';
 import AppWrapper from '../atoms/AppWrapper';
 import Colors from '../../styles/colors';
 import minWidth from '../../styles/mediaQuery';
 import useAuthContext from '../../hooks/useAuthContext';
 import useAppContext from '../../hooks/useAppContext';
 import ACTION_TYPES from '../../context/actionTypes';
-import googleLogo from '../../public/assets/google.svg';
-import { auth, googleAuthProvider, localPersistence } from '../../lib/firebase';
 
 const AppHeader = () => {
-  const [{ user }, dispatchAuth] = useAuthContext();
+  const [{ user }] = useAuthContext();
   const [{ isActiveNavDrawer }, dispatch] = useAppContext();
   const handleToggleNav = () => {
     dispatch({
       type: ACTION_TYPES.TOGGLE_DRAWER,
-    });
-  };
-
-  const handleGoogleAuth = () => {
-    auth.setPersistence(localPersistence).then(() => {
-      auth.signInWithPopup(googleAuthProvider).then((result) => {
-        const userAuthData = {
-          uid: result.user.uid,
-          displayName: result.user.displayName,
-          email: result.user.email,
-          photoURL: result.user.photoURL,
-        };
-
-        dispatchAuth({
-          type: ACTION_TYPES.SET_AUTH,
-          payload: userAuthData,
-        });
-      });
     });
   };
 
@@ -48,15 +26,9 @@ const AppHeader = () => {
         <HeaderWrapper>
           <AppLogo />
           <HeaderAction>
-            {!user && (
-              <AppButton type="button" white onClick={handleGoogleAuth}>
-                <Image src={googleLogo} alt="" />
-                <span>Login with google</span>
-              </AppButton>
-            )}
             {user && (
               <IconButton>
-                <Avatar />
+                <Avatar src={user.photoURL} />
               </IconButton>
             )}
             <MenuButton type="button" onClick={handleToggleNav}>
@@ -99,13 +71,11 @@ const HeaderAction = styled.div`
   .MuiButtonBase-root {
     margin-left: 1rem;
   }
-  .MuiAvatar-root {
-    border: 2px solid ${Colors.white};
-  }
 
   .MuiAvatar-root {
-    width: 2.5rem;
-    height: 2.5rem;
+    border: 2px solid ${Colors.white};
+    width: 2.7rem;
+    height: 2.7rem;
     cursor: pointer;
   }
 `;
