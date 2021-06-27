b29252ef69fc86f2f609734cd8b8749c8fc912f2
edwintantawi <edwintantawi@gmail.com>
2021-06-27 11:46:52 +0700

    🎨 protected route

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,12 +1,25 @@
+import { useRouter } from 'next/router';
 import AppShell from '../components/organisms/AppShell';
 import AppNavigation from '../components/molecules/AppNavigation';
 import AppTextEditor from '../components/organisms/AppTextEditor';
+import useAuthContext from '../hooks/useAuthContext';
+import useAppContext from '../hooks/useAppContext';
 
-const Dashboard = () => (
-  <AppShell>
-    <AppNavigation />
-    <AppTextEditor />
-  </AppShell>
-);
+const Dashboard = () => {
+  const [{ user }] = useAuthContext();
+  const [{ isLoading }] = useAppContext();
+  const router = useRouter();
+
+  if (!user && !isLoading) {
+    router.push('/');
+  }
+
+  return (
+    <AppShell>
+      <AppNavigation />
+      <AppTextEditor />
+    </AppShell>
+  );
+};
 
 export default Dashboard;
