ef42a5e6a607f7a31460adb969125c1c672b25bc
edwintantawi <edwintantawi@gmail.com>
2021-06-29 00:14:21 +0700

    🎨 use CSR and skeleton component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,35 +1,51 @@
+import { useEffect, useState } from 'react';
 import AppShell from '../components/organisms/AppShell';
 import AppBlogCard from '../components/atoms/AppBlogCard';
+import AppBlogCardSkeleton from '../components/atoms/AppBlogCardSkeleton';
 
-const Home = ({ posts }) => (
-  <AppShell>
-    <div>
-      {posts.results.map(({ id, data }, index) => (
-        <AppBlogCard
-          key={id}
-          url={`/blogposts/${id}`}
-          index={index}
-          data={{
-            bannerImage: data.bannerImage,
-            authorPicture: data.authorPicture,
-            authorName: data.authorName,
-            timestamp: data.timestamp,
-            title: data.title,
-            tags: data.tags,
-          }}
-        />
-      ))}
-    </div>
-  </AppShell>
-);
+const Home = () => {
+  const [blogposts, setBlogposts] = useState([]);
 
-export const getServerSideProps = async () => {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_DOMAIN}/api/blogposts`
+  const getBlogposts = async () => {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_DOMAIN}/api/blogposts`
+    );
+    const responseJson = await response.json();
+    setBlogposts(responseJson.results);
+  };
+
+  useEffect(() => {
+    getBlogposts();
+  }, []);
+
+  return (
+    <AppShell>
+      <div>
+        {!blogposts.length && (
+          <>
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((id) => (
+              <AppBlogCardSkeleton key={id} />
+            ))}
+          </>
+        )}
+        {blogposts.map(({ id, data }, index) => (
+          <AppBlogCard
+            key={id}
+            url={`/blogposts/${id}`}
+            index={index}
+            data={{
+              bannerImage: data.bannerImage,
+              authorPicture: data.authorPicture,
+              authorName: data.authorName,
+              timestamp: data.timestamp,
+              title: data.title,
+              tags: data.tags,
+            }}
+          />
+        ))}
+      </div>
+    </AppShell>
   );
-  const responseJson = await response.json();
-
-  return { props: { posts: responseJson } };
 };
 
 export default Home;
