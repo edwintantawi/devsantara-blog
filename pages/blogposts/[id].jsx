80568a786bc3ec53d82252f84a2c69addee37cd6
edwintantawi <edwintantawi@gmail.com>
2021-06-28 12:29:20 +0700

    🐛 fix bad css structur

--- pages/blogposts/[id].jsx
+++ pages/blogposts/[id].jsx
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 import StarterKit from '@tiptap/starter-kit';
 import AppShell from '../../components/organisms/AppShell';
 import AppActionBlogPost from '../../components/atoms/AppActionBlogPost';
-import minWidth from '../../styles/mediaQuery';
 import Colors from '../../styles/colors';
 
 const BlogPost = ({ blogpost }) => (
@@ -40,101 +39,95 @@ export const getStaticProps = async ({ params: { id } }) => {
 };
 
 const ContentLayout = styled.main`
+  > * + * {
+    margin-top: 1rem;
+  }
 
-  @media ${minWidth('md')} {
-      /* padding: 3rem; */
-    }
+  * {
+    font-size: 1.1rem;
+    line-height: 1.7;
+    letter-spacing: 1px;
+  }
 
-    > * + * {
-      margin-top: 1rem;
-    }
+  ul,
+  ol {
+    padding: 0 1rem;
+  }
 
-    * {
-      font-size: 1.1rem;
-      line-height: 1.7;
-      letter-spacing: 1px;
-    }
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    line-height: 1.2;
+    margin-top: 2.5rem;
+  }
 
-    ul,
-    ol {
-      padding: 0 1rem;
-    }
+  h1 {
+    font-size: 2.625rem;
+  }
+  h2 {
+    font-size: 2rem;
+  }
+  h3 {
+    font-size: 1.4rem;
+  }
+  h4 {
+    font-size: 1.3rem;
+  }
+  h5 {
+    font-size: 1.1rem;
+  }
+  h6 {
+    font-size: 1rem;
+  }
 
-    h1,
-    h2,
-    h3,
-    h4,
-    h5,
-    h6 {
-      line-height: 1.2;
-      margin-top: 2.5rem;
-    }
+  code {
+    display: inline-block;
+    padding: 0.2rem 0.6rem;
+    background-color: #61616132;
+    color: #424242;
+    font-family: 'JetBrainsMono', monospace;
+  }
 
-    h1 {
-      font-size: 2.625rem;
-    }
-    h2 {
-      font-size: 2rem;
-    }
-    h3 {
-      font-size: 1.4rem;
-    }
-    h4 {
-      font-size: 1.3rem;
-    }
-    h5 {
-      font-size: 1.1rem;
-    }
-    h6 {
-      font-size: 1rem;
-    }
+  pre {
+    background: #0d0d0d;
+    color: #fff;
+    font-family: 'JetBrainsMono', monospace;
+    padding: 0.75rem 1rem;
+    border-radius: 0.5rem;
+    max-width: 1320px;
+    overflow: auto;
 
     code {
-      display: inline-block;
-      padding: 0.2rem 0.6rem;
-      background-color: #61616132;
-      color: #424242;
-      font-family: 'JetBrainsMono', monospace;
+      color: inherit;
+      padding: 0;
+      background: none;
+      font-size: 1rem;
     }
+  }
 
-    pre {
-      background: #0d0d0d;
-      color: #fff;
-      font-family: 'JetBrainsMono', monospace;
-      padding: 0.75rem 1rem;
-      border-radius: 0.5rem;
-      max-width: 1320px;
-      overflow: auto;
+  hr {
+    height: 1px;
+    background-color: ${Colors.mediumGray};
+  }
 
-      code {
-        color: inherit;
-        padding: 0;
-        background: none;
-        font-size: 1rem;
-      }
-    }
+  img {
+    max-width: 100%;
+    height: auto;
+  }
 
-    hr {
-      height: 1px;
-      background-color: ${Colors.mediumGray};
-    }
+  blockquote {
+    padding-left: 1rem;
+    border-left: 3px solid ${Colors.mediumGray};
+    color: ${Colors.gray};
+  }
 
-    img {
-      max-width: 100%;
-      height: auto;
-    }
-
-    blockquote {
-      padding-left: 1rem;
-      border-left: 3px solid ${Colors.mediumGray};
-      color: ${Colors.gray};
-    }
-
-    hr {
-      border: none;
-      border-top: 2px solid rgba(#0d0d0d, 0.1);
-      margin: 2rem 0;
-    }
+  hr {
+    border: none;
+    border-top: 2px solid rgba(#0d0d0d, 0.1);
+    margin: 2rem 0;
   }
 `;
 
