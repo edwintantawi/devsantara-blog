cbd11a00aa5e4657797f920bf97d268a6a0641a0
edwintantawi <edwintantawi@gmail.com>
2021-06-29 12:35:17 +0700

    ✨ create loader component

--- components/atoms/AppLoader.jsx
+++ components/atoms/AppLoader.jsx
@@ -1,14 +1,35 @@
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Colors from '../../styles/colors';
+import AppButton from './AppButton';
 
-const AppLoader = () => (
-  <LoaderLayout>
-    <div>
-      <span>Loading...</span>
-      <p>show all over the world</p>
-    </div>
-  </LoaderLayout>
-);
+const AppLoader = ({ type, message, onClick }) => {
+  if (type === 'loading') {
+    return (
+      <LoaderLayout>
+        <div>
+          <span>Loading...</span>
+          <p>{message}</p>
+        </div>
+      </LoaderLayout>
+    );
+  }
+  if (type === 'error') {
+    return (
+      <LoaderLayout>
+        <div>
+          <span>Error...</span>
+          <p>{message}</p>
+          <AppButton type="button" className="full" onClick={onClick}>
+            Try Again
+          </AppButton>
+        </div>
+      </LoaderLayout>
+    );
+  }
+
+  return null;
+};
 
 const LoaderLayout = styled.div`
   position: fixed;
@@ -17,8 +38,8 @@ const LoaderLayout = styled.div`
   bottom: 0;
   right: 0;
   z-index: 99;
-  background-color: rgba(0, 0, 0, 0.6);
-  backdrop-filter: blur(5px);
+  padding: 1rem;
+  background-color: ${Colors.darkBlue};
   display: grid;
   place-items: center;
 
@@ -27,17 +48,34 @@ const LoaderLayout = styled.div`
     place-items: center;
     background-color: ${Colors.white};
     border-radius: 8px;
-    padding: 1rem 2rem;
+    padding: 1rem 1rem;
+    width: 100%;
+    max-width: 250px;
+    text-align: center;
 
     span {
       font-weight: 600;
-      font-size: 1.5rem;
+      font-size: 1.3rem;
     }
     p {
       color: ${Colors.gray};
-      font-size: 1rem;
+      font-size: 0.9rem;
+    }
+
+    button {
+      margin-top: 1rem;
     }
   }
 `;
+
+AppLoader.propTypes = {
+  type: PropTypes.oneOf(['loading', 'error']).isRequired,
+  message: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+};
+
+AppLoader.defaultProps = {
+  onClick: null,
+};
 
 export default AppLoader;
