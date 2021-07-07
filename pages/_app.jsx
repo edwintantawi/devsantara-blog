61170096ee25033114f53385a7d77cb84fb025bd
edwintantawi <edwintantawi@gmail.com>
2021-07-07 15:40:28 +0700

    :sparkles: Introducing new features (google analytics).

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,19 +1,28 @@
+import { useEffect } from 'react';
 import GlobalStyle from '../styles/globalStyle';
 import AuthContextProvider from '../context/authContext/store';
 import AppContextProvider from '../context/appContext/store';
 import AppAuthLayer from '../components/atoms/AppAuthLayer';
+import { analytics } from '../lib/firebase';
 
-const MyApp = ({ Component, pageProps }) => (
-  <>
-    <GlobalStyle />
-    <AuthContextProvider>
-      <AppContextProvider>
-        <AppAuthLayer>
-          <Component {...pageProps} />
-        </AppAuthLayer>
-      </AppContextProvider>
-    </AuthContextProvider>
-  </>
-);
+const MyApp = ({ Component, pageProps }) => {
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      analytics();
+    }
+  }, []);
+  return (
+    <>
+      <GlobalStyle />
+      <AuthContextProvider>
+        <AppContextProvider>
+          <AppAuthLayer>
+            <Component {...pageProps} />
+          </AppAuthLayer>
+        </AppContextProvider>
+      </AuthContextProvider>
+    </>
+  );
+};
 
 export default MyApp;
